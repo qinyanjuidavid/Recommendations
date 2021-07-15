5cf3ca85dac10be1f59acbf44661577fb513b7cf
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-07-15 18:09:18 +0300

    Coming up with the function for generating the refarral code

--- Profiles/admin.py
+++ Profiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from Profiles.models import Profile
+
+
 # Register your models here.
+@admin.register(Profile)
+class MyAdmin(admin.ModelAdmin):
+    list_display = ("code",)
